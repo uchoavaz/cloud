8d7f3cf9fddb2bcdae4e6bf4fc45b4a18afb5e56
uchoavaz <joaouchoavaz@hotmail.com>
2016-06-27 19:13:28 -0300

    adicao de commentários no vm_lib.py

--- cloud/vm/vm_lib.py
+++ cloud/vm/vm_lib.py
@@ -3,6 +3,13 @@ import os
 import socket
 
 COMMANDS = {
+
+    """
+    Esta variável consiste em armazenar um dicionário
+    dos comandos que poderão ser escritos no VagranFile de
+    cada máquina virtual peo Vagrant
+    """
+
     'configinit': 'Vagrant.configure(2) do |config|\n',
     'vmname': '  config.vm.box = "{0}"\n',
     'vmip': '  config.vm.network "public_network", bridge: "wlan0", ip: "{0}" \n',
@@ -26,8 +33,16 @@ COMMANDS = {
 
 
 class Disk():
+    """
+    Esta classe é responsável por modificar o tamanho do
+    disco de uma determinada máquina virtual
+    """
 
     def set_disk(self, disk, vm_name, atual_path):
+        """
+        Este método é responsável por definir qual o tamanho que o
+        usuário deseja setar em uma maquina virtual passada para ele
+        """
         new_disk = 'resized-disk1.vdi'
         old_disk = 'box-disk1.vmdk'
         os.chdir(
@@ -62,6 +77,10 @@ class Disk():
 
 
 class VMCreation(Disk):
+    """
+    Esta classe é reponsável por criar uma máquina virtual
+    pelo vagrant
+    """
     name = None
     ip = None
     image = None
@@ -72,6 +91,10 @@ class VMCreation(Disk):
     initial_path = None
 
     commands = {
+        """
+        Esta variável define os comandos que realmente 
+        serão setados no Vagranfile
+        """
         'configinit': COMMANDS['configinit'],
         'endconfigmachine': COMMANDS['endconfigmachine'],
         'configmachine': COMMANDS['configmachine'],
@@ -81,6 +104,10 @@ class VMCreation(Disk):
     }
 
     VARIABLES = [
+        """
+        Esta lista representa qual será a ordem de escrita de cada
+        variável definida em "commands"
+        """
         'configinit',
         'vmname',
         'vmip',
@@ -100,6 +127,9 @@ class VMCreation(Disk):
     ]
 
     def vm_name(self, name):
+        """
+        Define o nome da VM
+        """
         self.commands['vmname'] = COMMANDS['vmname'].format(name)
         self.commands['vname'] = COMMANDS['vname'].format(name)
         self.commands['vmmachinename'] = COMMANDS['vmmachinename'].format(
@@ -107,28 +137,50 @@ class VMCreation(Disk):
         self.name = name
 
     def vm_ip(self, ip):
+        """
+        Define o ip da máquina na rede privada
+        """
         self.commands['vmip'] = COMMANDS['vmip'].format(ip)
         self.ip = ip
 
     def vm_cpu(self, cpu):
+        """
+        Define a quantidade de core da VM
+        """
         self.commands['vmcpu'] = COMMANDS['vmcpu'].format(cpu)
         self.cpu = cpu
 
     def vm_memory(self, memory):
+        """
+        Define a memória da máquina em MB
+        """
         self.commands['vmmemory'] = COMMANDS['vmmemory'].format(memory)
         self.memory = memory
 
     def vm_path_image(self, image):
+        """
+        Define onde está a imagem para contruir a VM
+        """
         self.image = os.getcwd() + "/" + image
 
     def vm_disk(self, disk):
+        """
+        Define o tamanho do disco na VM
+        """
         self.disk = disk
 
     def vm_password(self, password):
+        """
+        Define a senha da conexao ssh com a VM
+        """
         self.commands['vmpassword'] = COMMANDS['vmpassword'].format(password)
         self.password = password
 
     def create(self):
+        """
+        Este método executa automaticamente os processos de criação 
+        da vm listados abaixo
+        """
         self.initial_path = os.getcwd()
         self.create_folder(self.name)
         self.create_vm()
@@ -136,6 +188,9 @@ class VMCreation(Disk):
         os.chdir(self.initial_path)
 
     def create_folder(self, folder_name):
+        """
+        Cria a a pasta no computador raiz referente a VM em instância
+        """
         if not os.path.isdir("machines"):
             os.system("mkdir machines")
         os.chdir(os.getcwd() + "/machines")
@@ -145,10 +200,16 @@ class VMCreation(Disk):
         os.chdir(folder)
 
     def create_vm(self):
+        """
+        Executa o comando de criação da VM
+        """
         command = "vagrant box add {0} {1}".format(self.name, self.image)
         os.system(command)
 
     def create_vagrantfile(self):
+        """
+        Cria o VagrantFile dentro da pasta listada acima
+        """
         os.system("vagrant init")
         vagrantfile = open("Vagrantfile", "w")
 
@@ -159,6 +220,11 @@ class VMCreation(Disk):
         os.system("vagrant up")
 
         if self.disk > 40960:
+            """
+            O vagrant, por padrão, cria a máquina com 40 GB. Caso a máquina
+            seja maior de este valor, ele pausa a máquina faz o aumento do
+            disco e sobe a máquina novamente
+            """
             os.system("vagrant halt")
             vm_path = os.getcwd()
             self.set_disk(self.disk, self.name, vm_path)
@@ -166,14 +232,23 @@ class VMCreation(Disk):
 
 
 class VMDeletion():
+    """
+    Esta classe é reponsável por deletar do computador raiz uma VM antes já criada
+    """
     initial_path = None
     name = None
 
     def vm_name(self, name):
+        """
+        Define o nome da VM
+        """
         self.name = name
         self.initial_path = os.getcwd()
 
     def remove(self):
+        """
+        Remove a VM da máquina e todos os seus vestígios
+        """
         halt_command = "vagrant halt"
         destroy_command = 'vagrant destroy --f'
         delete_folder_command = "rm -rf {0}"
@@ -197,6 +272,10 @@ class VMDeletion():
 
 
 class VMEdition(Disk):
+    """
+    Esta classe é responsável por editar/alterar as configurações de 
+    uma VM antes já criada
+    """
     name = None
     ip = None
     image = None
@@ -206,6 +285,10 @@ class VMEdition(Disk):
     initial_path = None
 
     commands = {
+        """
+        Esta variável define os comandos que realmente
+        serão setados no Vagranfile
+        """
         'configinit': COMMANDS['configinit'],
         'endconfigmachine': COMMANDS['endconfigmachine'],
         'configmachine': COMMANDS['configmachine'],
@@ -214,6 +297,11 @@ class VMEdition(Disk):
     }
 
     VARIABLES = [
+        """
+        Esta lista representa qual será a ordem de escrita de cada
+        variável definida em "commands"
+        """
+
         'configinit',
         'vmname',
         'vmip',
@@ -227,8 +315,10 @@ class VMEdition(Disk):
         'vmprovisionend'
     ]
 
-
     def vm_name(self, name):
+        """
+        Define o nome da VM
+        """
         self.commands['vmname'] = COMMANDS['vmname'].format(name)
         self.commands['vname'] = COMMANDS['vname'].format(name)
         self.commands['vmmachinename'] = COMMANDS['vmmachinename'].format(
@@ -236,21 +326,37 @@ class VMEdition(Disk):
         self.name = name
 
     def vm_ip(self, ip):
+        """
+        Define o ip da máquina na rede privada
+        """
         self.commands['vmip'] = COMMANDS['vmip'].format(ip)
         self.ip = ip
 
     def vm_cpu(self, cpu):
+        """
+        Define a quantidade de core da VM
+        """
         self.commands['vmcpu'] = COMMANDS['vmcpu'].format(cpu)
         self.cpu = cpu
 
     def vm_memory(self, memory):
+        """
+        Define a memória da máquina em MB
+        """
         self.commands['vmmemory'] = COMMANDS['vmmemory'].format(memory)
         self.memory = memory
 
     def vm_disk(self, disk):
+        """
+        Define o tamanho do disco na VM
+        """
         self.disk = disk
 
     def reset_password(self, password):
+        """
+        Reseta a senha de conexão ssh com a VM
+        Também refaz o VagranFile
+        """
         variables = [
             'configinit',
             'vmname',
@@ -287,6 +393,10 @@ class VMEdition(Disk):
         os.chdir(self.initial_path)
 
     def edit(self):
+        """
+        Executa os comandos de dar pausa na VM para subir as modificações
+        para a VM
+        """
         self.initial_path = os.getcwd()
 
         if os.path.isdir("machines"):
